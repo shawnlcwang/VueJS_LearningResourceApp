c226da9b77c732e1d0e551506233a234b9c605a5
Shawn <swang@foundryspatial.com>
2020-10-27 20:28:18 -0700

    commit update

---
+++ src/components/learning-resources/LearningResource.vue
@@ -0,0 +1,60 @@
+<template>
+  <li>
+    <base-card>
+      <template #default>
+        <header>
+          <h3>{{ title }}</h3>
+          <base-button
+            mode="flat"
+            @click="deleteResource(id)"
+          >
+            Delete
+          </base-button>
+        </header>
+        <p>{{ description }}</p>
+        <nav>
+          <a :href="link">View Resource</a>
+        </nav>
+      </template>
+    </base-card>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ['id', 'title', 'description', 'link'],
+  inject: ['deleteResource']
+}
+</script>
+
+<style scoped>
+li {
+  margin: auto;
+  max-width: 40rem;
+}
+
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+h3 {
+  font-size: 1.25rem;
+  margin: 0.5rem 0;
+}
+
+p {
+  margin: 0.5rem 0;
+}
+
+a {
+  text-decoration: none;
+  color: #ce5c00;
+}
+
+a:hover,
+a:active {
+  color: #c89300;
+}
+</style>
